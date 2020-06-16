9e36f7639a59e243b37b34fce6c20786d71016e7
DuhonTheGuy <53868103+DuhonTheGuy@users.noreply.github.com>
2020-06-16 08:54:47 -0400

    Added skill argument, made more "yes" answers, added easteregg.

--- src/main/java/com/example/eightBallPlugin.java
+++ src/main/java/com/example/eightBallPlugin.java
@@ -2,10 +2,13 @@ package com.example;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
+import jdk.internal.joptsimple.internal.Strings;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.CommandExecuted;
 import net.runelite.api.events.GameStateChanged;
@@ -25,8 +28,8 @@ import java.util.Random;
 )
 public class eightBallPlugin extends Plugin
 {
-	List<String> answerList = Arrays.asList("Definitely.", "Nope.", "Yes.", "Nah.", "Ask me later.", "All I can say is yes.", "Pretty sure it's a yes.", "Pretty sure it's a no.", "I can't answer that.", "That is never going to happen.", "You are legit asking this to an rng plugin?", "Maybe.");
-
+	List<String> answerList = Arrays.asList("Definitely.", "Nope.", "Yes.", "Nah.", "Ask me later.", "All I can say is yes.", "Pretty sure it's a yes.", "Pretty sure it's a no.", "I can't answer that.", "That is never going to happen.", "You are legit asking this to an rng plugin?", "Maybe.", "This will 100% happen.", "Surely.", "The rng Gods will smile to you.", "Bruh, of course not lmaoooo");
+	List<String> skillList = Arrays.asList("Attack", "Strength", "Defense", "Ranged", "Prayer", "Magic", "Runecraft, let me see you   s u f f e r", "Hitpoints", "Crafting", "Mining", "Smithing", "Fishing", "Cooking", "Firemaking", "Woodcutting");
 	@Inject
 	private Client client;
 
@@ -57,8 +60,22 @@ public class eightBallPlugin extends Plugin
 	public void onCommandExecuted(CommandExecuted commandExecuted){
 		if (commandExecuted.getCommand().equals("8ball")) {
 			Random rand = new Random();
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The 8ball says: " + answerList.get(rand.nextInt(answerList.size())) , null);
-
+			if (commandExecuted.getArguments().length != 0 && commandExecuted.getArguments()[0].equals("skill"))
+			{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The 8ball says: " + skillList.get(rand.nextInt(skillList.size())), null);
+			}
+			else if (WorldPoint.fromLocalInstance(client, client.getLocalPlayer().getLocalLocation()).getRegionID() == 0)
+			{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The 8ball says: Wtf dude how are you even here-", null);
+			}
+			else if (WorldPoint.fromLocalInstance(client, client.getLocalPlayer().getLocalLocation()).getRegionID() == 12597)
+			{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The 8ball says: Drink the forbidden berry fruit punch, I double dare ya.", null);
+			}
+			else
+			{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "The 8ball says: " + answerList.get(rand.nextInt(answerList.size())), null);
+			}
 		}
 	}
 
