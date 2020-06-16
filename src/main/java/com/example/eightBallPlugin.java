a86be3210151d4ed9f8bf8db47a1d9da0a4b74b6
DuhonTheGuy <53868103+DuhonTheGuy@users.noreply.github.com>
2020-06-16 10:14:57 -0400

    No more string import.

--- src/main/java/com/example/eightBallPlugin.java
+++ src/main/java/com/example/eightBallPlugin.java
@@ -3,7 +3,6 @@ package com.example;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 
-import jdk.internal.joptsimple.internal.Strings;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
